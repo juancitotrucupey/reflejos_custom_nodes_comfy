0f78f1a03c043939f8a97ab8d5e987d39c817dcd
Agent Smith <jsflorezj@gmail.com>
2025-01-10 13:58:15 -0500

    Add the date of submission to the name of the workflow and parameters file

--- custom_node_creating_folders/node.py
+++ custom_node_creating_folders/node.py
@@ -10,6 +10,7 @@ class ReflejosResultsOrganizer:
                 "exp_folder_name": ("STRING", {"default": ""}),
                 "wf_api_json": ("STRING", {"default": '\{\}'}),
                 "wf_params_dict": ("STRING", {"default": "\{\}"}),
+                "time_of_submission": ("STRING", {"default": ""}),
             }
         }
 
@@ -23,19 +24,26 @@ class ReflejosResultsOrganizer:
             wf_folder_name:str, 
             exp_folder_name:str,
             wf_api_json:str,
-            wf_params_dict:str     
+            wf_params_dict:str,
+            time_of_submission:str     
         ):
-        # Create the folder if it doesn't exist
+        # Create the folder for the workflow resutls if it doesn't exist
         if not os.path.exists(wf_folder_name):
             os.makedirs(wf_folder_name)
 
-        # Create the folder if it doesn't exist
+        # Create the folder of the experiment results if it doesn't exist
         if not os.path.exists(exp_folder_name):
             os.makedirs(exp_folder_name)
 
+        # Create the folder for storing the workflows and parameters json
+        workflow_info_folder = os.path.join(exp_folder_name, "workflow__info")
+        if not os.path.exists(workflow_info_folder):
+            os.makedirs(workflow_info_folder)
+    
+
         # Create the file path
-        wf_api_path = os.path.join(wf_folder_name, "workflow_api.json")
-        wf_params_path = os.path.join(exp_folder_name, "workflow_params.json")
+        wf_api_path = os.path.join(workflow_info_folder, f"workflow_api_{time_of_submission}.json")
+        wf_params_path = os.path.join(workflow_info_folder, f"workflow_params_{time_of_submission}.json")
 
         # Write the content to the file
         json.dump(json.loads(wf_api_json), open(wf_api_path, "w"), indent=4)
